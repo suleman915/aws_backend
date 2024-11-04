9625d0d2a28290a174bb4b10d313f1d72d12ecc3
tanzeelRehman <tanzeel@referralynx.com>
2024-11-04 20:49:41 +0500

    Resolved merge conflicts

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.post('/api/save', (req, res) => {
     res.json({ success: true, message: 'Message saved successfully!' });
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
+
